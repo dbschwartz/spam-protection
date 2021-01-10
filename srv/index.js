34e23a93a07f16475fcf45fe5ec3bd736fe65e0e
David Schwartz <dbschwartz2@gmail.com>
2021-01-10 14:31:50 -0600

    backend finished

--- srv/index.js
+++ srv/index.js
@@ -1,27 +1,39 @@
 import express from 'express';
 const fs = require('fs').promises;
+const path = require("path");
 
-export default (app) => {
+
+const getReports = async (next) => {
+  try {
+
+    const unparsedJSON = await fs.readFile(path.resolve(__dirname,'./reports.json'), 'utf8');
+    return JSON.parse(unparsedJSON);
+    //const reports = require("./reports.json")
+    //return reports;
+  } catch (error) {
+    next(error)
+  }
+}
+
+export default async (app) => {
   app.use(express.json());
-  const reports = require('../data/reports.json')
 
-  app.get('/reports', (req, res) => {
-    res.json(reports)
+  app.get('/reports', async(req, res, next) => {
+    const reports = await getReports(next);
+    res.json(reports.elements.map(({id, state}) => ({id, state})));
   });
 
   app.put('/reports/:id', async (req, res, next) => {
-    const element = reports.find( ({ id }) => id === req.params.id );
-    element.status = req.body.ticketState;
+    const reports = await getReports(next);
+    const element = reports.elements.find( ({ id }) => id === req.params.id );
+    element.state = req.body.ticketState;
     try {
-      await fs.writeFile('../data/reports.json', JSON.stringify(reports, null, 4)); // need to be in an async function
+      await fs.writeFile('./srv/reports.json', JSON.stringify(reports, null, 4), 'utf8'); 
+      //fsSync.writeFileSync('student-2.json', JSON.stringify(reports, null, 4));
       res.send('Succesfully updated reports!')
     } catch (error) {
       next(error)
     }
   });
-  
-  app.post('/bar', (req, res) => {
-    res.json(req.body);
-  });
-  
+    
 }
